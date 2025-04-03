2f685894e012857e5ea4782fc04634ba4a465ed2
Garimajunejaa <junejag51@gmail.com>
2025-04-03 15:44:02 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let checkbox = document.querySelector("input[name=theme]");
+const checkbox = document.querySelector("input[name=theme]");
 
-checkbox.addEventListener("change", function () {
+// Check for saved theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    checkbox.checked = true;
+  }
+}
+
+// Theme toggle handler
+checkbox.addEventListener("change", function() {
   if (this.checked) {
-    trans();
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
   } else {
-    trans();
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
   }
 });
-
-let trans = () => {
-  console.log(document.documentElement);
-  document.documentElement.classList.add("transition");
-  window.setTimeout(() => {
-    document.documentElement.classList.remove("transition");
-  }, 1000);
-};
